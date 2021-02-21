023c5e535cacaad9245b1ef4bbe27a2279e35153
Axel Hodler <axel@hodler.co>
2021-02-21 15:16:23 +0100

    adding ECR repository

--- test/cdktests.test.ts
+++ test/cdktests.test.ts
@@ -1,13 +1,13 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Cdktests from '../lib/cdktests-stack';
 
-test('Empty Stack', () => {
+test('ECR Resource is present', () => {
     const app = new cdk.App();
-    // WHEN
+
     const stack = new Cdktests.CdktestsStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+    expectCDK(stack).to(haveResource("AWS::ECR::Repository",{
+        RepositoryName: 'test-repository'
+    }));
 });
