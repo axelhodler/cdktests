6816c27d918076be21fe3ec0db13507cfe3a81ed
Axel Hodler <axel@hodler.co>
2021-02-26 06:45:39 +0100

    add albfs with healthcheck

--- lib/cdktests-stack.ts
+++ lib/cdktests-stack.ts
@@ -1,12 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 import * as ecr from '@aws-cdk/aws-ecr';
+import * as ecs from '@aws-cdk/aws-ecs';
+import {ApplicationLoadBalancedFargateService} from "@aws-cdk/aws-ecs-patterns";
 
 export class CdktestsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new ecr.Repository(this, 'test-repository', {
+    const repository = new ecr.Repository(this, 'test-repository', {
       repositoryName: `test`
+    });
+
+    const albfs = new ApplicationLoadBalancedFargateService(this, 'test-fargate', {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(repository, 'latest')
+      }
+    })
+
+    albfs.targetGroup.configureHealthCheck({
+      path: '/actuator/health',
     });
   }
 }
--- test/cdktests.test.ts
+++ test/cdktests.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Cdktests from '../lib/cdktests-stack';
 
@@ -8,6 +8,88 @@ test('ECR Resource is present', () => {
     const stack = new Cdktests.CdktestsStack(app, 'MyTestStack');
 
     expectCDK(stack).to(haveResource("AWS::ECR::Repository",{
-        RepositoryName: 'test-repository'
+        RepositoryName: 'test'
+    }));
+});
+
+test('Fargate service is using the default values', () => {
+    const app = new cdk.App();
+
+    const stack = new Cdktests.CdktestsStack(app, 'MyTestStack');
+
+    expectCDK(stack).to(haveResource("AWS::ECS::Service",{
+        LaunchType: 'FARGATE',
+        DesiredCount: 1,
+        HealthCheckGracePeriodSeconds: 60
+    }));
+});
+
+test('Fargate is using the latest image in ECR', () => {
+    const app = new cdk.App();
+
+    const stack = new Cdktests.CdktestsStack(app, 'MyTestStack');
+
+    expectCDK(stack).to(haveResourceLike("AWS::ECS::TaskDefinition",{
+        ContainerDefinitions: [{
+            Image: {
+                "Fn::Join": [
+                    "",
+                    [
+                        {
+                            "Fn::Select": [
+                                4,
+                                {
+                                    "Fn::Split": [
+                                        ":",
+                                        {
+                                            "Fn::GetAtt": [
+                                                "testrepository3CFC24EC",
+                                                "Arn"
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        ".dkr.ecr.",
+                        {
+                            "Fn::Select": [
+                                3,
+                                {
+                                    "Fn::Split": [
+                                        ":",
+                                        {
+                                            "Fn::GetAtt": [
+                                                "testrepository3CFC24EC",
+                                                "Arn"
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        ".",
+                        {
+                            "Ref": "AWS::URLSuffix"
+                        },
+                        "/",
+                        {
+                            "Ref": "testrepository3CFC24EC"
+                        },
+                        ":latest"
+                    ]
+                ]
+            }
+        }]
+    }));
+});
+
+test('ELB health check path is using Spring Boot endpoint', () => {
+    const app = new cdk.App();
+
+    const stack = new Cdktests.CdktestsStack(app, 'MyTestStack');
+
+    expectCDK(stack).to(haveResource("AWS::ElasticLoadBalancingV2::TargetGroup",{
+        HealthCheckPath: '/actuator/health'
     }));
 });
